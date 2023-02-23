40f657edce50b06b6562de0fb2a88619a84fac96
Piotr Paterek <paterekpiotr123@gmail.com>
2023-02-23 20:14:13 +0100

    Stworzenie interface użytkownika i pobieranie map z pliku tekstowego

--- First2DGame/Program.cs
+++ First2DGame/Program.cs
@@ -1,122 +1,48 @@
-﻿namespace First2DGame
+﻿using System;
+using System.Text;
+
+namespace First2DGame
 {
     public class Program
     {
-        private static int PlayerX = 2;
-        private static int PlayerY = 2;
-
-        private static readonly int[,] map =
-        {
-            { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, },
-            { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, },
-            { 1, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, },
-            { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, },
-            { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, },
-            { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, },
-            { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, },
-            { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, },
-            { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, },
-            { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, },
-            { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, },
-            { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, }
-        };
-        
-        public static void DrawMap()
-        {
-            for (int y = 0; y < map.GetLength(1); y++)
-            {
-                for (int x = 0; x < map.GetLength(0); x++)
-                {
-                    Console.CursorLeft = y;
-                    Console.CursorTop = x;
-                    if (map[x, y] == 1)
-                    {
-                        Console.BackgroundColor = ConsoleColor.Magenta;
-                        Console.ForegroundColor = ConsoleColor.Magenta;
-                        Console.Write("#");
-                        Console.BackgroundColor = ConsoleColor.Black;
-                        Console.ForegroundColor = ConsoleColor.White;
-                    }
-                    else if (map[x, y] == 4)
-                    {
-                        PlayerX = y;
-                        PlayerY = x;
-                    }
-
-                }
-            }
-        }
-
-        public static bool PlayerCanMove(int x, int y)
-        {
-            if (map[y, x] != 1) return true;
-            else return false;
-        }
-        public static void DrawPlayer()
-        {
-            Console.CursorLeft = PlayerX;
-            Console.CursorTop = PlayerY;
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.Write((char) 2);
-            Console.ForegroundColor = ConsoleColor.White;
-        }
-        public static void HidePlayer()
-        {
-            Console.CursorLeft = PlayerX;
-            Console.CursorTop = PlayerY;
-            Console.Write(" ");
-        }
         public static void Main(string[] args)
         {
-            Console.Title = "title";
-            DrawMap();
+            /*Console.CursorVisible = false;
+            UserInterface userInterface = new UserInterface();
             while (true)
             {
-                Console.CursorVisible = false;
-                DrawPlayer();
+                userInterface.SelectingOptions();
+            }*/
 
-                ConsoleKeyInfo key = Console.ReadKey(true);
-                if (key.Key == ConsoleKey.W && PlayerY > 0)
+
+
+
+            string data = File.ReadAllText("Levels/level1.txt", Encoding.UTF8);
+
+            String[] x = data.Split(";");
+
+            int[][] result = new int[x.Length][];
+
+            for (int i = 0; i < x.Length; i++)
+            {
+                String[] row = x[i].Split(",");
+                result[i] = new int[row.Length];
+
+                for (int j = 0; j < row.Length; j++)
                 {
-                    if (PlayerCanMove(PlayerX, PlayerY-1))
-                    {
-                        HidePlayer();
-                        PlayerY--;
-                        DrawPlayer();
-                    }
-                }
-                else if (key.Key == ConsoleKey.S)
-                {
-                    if (PlayerCanMove(PlayerX, PlayerY+1))
-                    {
-                        HidePlayer();
-                        PlayerY++;
-                        DrawPlayer();
-                    }
-                }
-                else if (key.Key == ConsoleKey.A && PlayerX > 0)
-                {
-                    if (PlayerCanMove(PlayerX-1, PlayerY))
-                    {
-                        HidePlayer();
-                        PlayerX--;
-                        DrawPlayer();
-                    }
-                }
-                else if (key.Key == ConsoleKey.D)
-                {
-                    if (PlayerCanMove(PlayerX+1, PlayerY))
-                    {
-                        HidePlayer();
-                        PlayerX++;
-                        DrawPlayer();
-                    }
-                }
-                if(key.Key == ConsoleKey.Escape)
-                {
-                    break;
+                    result[i][j] = Int32.Parse(row[j]);
                 }
             }
+
+            foreach (int[] tab in result)
+            {
+                foreach (int licz in tab)
+                {
+                    Console.Write(licz);
+                }
+                Console.WriteLine();
+            }
+
         }
     }
 }
---
+++ First2DGame/UserInterface.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First2DGame
+{
+    class UserInterface
+    {
+        public String[] menu =
+        {
+            "New Game",
+            "Load Game",
+            "Settings",
+            "Exit"
+        };
+        static int x = 0;
+
+        public void DrawMenu(int x)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("=======================================================================\r\n" +
+                          "   /__  ___/                     //   ) )                              \r\n" +
+                          "     / /  / __      ___         //         ___      _   __      ___    \r\n" +
+                          "    / /  //   ) ) //___) )     //  ____  //   ) ) // ) )  ) ) //___) ) \r\n" +
+                          "   / /  //   / / //           //    / / //   / / // / /  / / //        \r\n" +
+                          "  / /  //   / / ((____       ((____/ / ((___( ( // / /  / / ((____     \r\n" +
+                          "=======================================================================\r\n\n");
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            for (int i = 0; i < menu.Length; i++)
+            {
+                if (x >= 0 && i < menu.Length)
+                {
+                    if (x == i)
+                    {
+                        if(i == menu.Length - 1) Console.ForegroundColor = ConsoleColor.Red; 
+                        else Console.ForegroundColor = ConsoleColor.Green; 
+
+                        Console.WriteLine(" [ " + menu[i] + " ]");
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                    }
+                    else
+                    {
+                        Console.WriteLine("   " + menu[i]);
+                    }
+                }
+            }
+        }
+        public void SelectingOptions()
+        {
+            Console.Clear();
+            DrawMenu(x);
+
+            ConsoleKeyInfo key = Console.ReadKey(true);
+            if ((key.Key == ConsoleKey.W || key.Key == ConsoleKey.UpArrow) && x > 0) x--;
+            else if ((key.Key == ConsoleKey.S || key.Key == ConsoleKey.DownArrow) && x < menu.Length - 1) x++;
+            else if (key.Key == ConsoleKey.Enter || key.Key == ConsoleKey.Escape)
+            {
+                Console.Clear();
+                Click(key);
+                return;
+            }
+
+            Console.Clear();
+            DrawMenu(x);
+        }
+        public void Click(ConsoleKeyInfo key)
+        {
+            if(x == 0)
+            {
+
+            }
+            else if (x == 1)
+            {
+
+            }
+            else if (x == 2)
+            {
+
+            }
+            else if (x == 3)
+            {
+                Console.ForegroundColor = ConsoleColor.Black;
+                Environment.Exit(0);
+            }
+        }
+    }
+}
