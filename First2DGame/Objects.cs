93b39697cfde6577bee2f9efca3debdb0c88248b
Piotr Paterek <paterekpiotr123@gmail.com>
2023-02-27 20:24:42 +0100

    x

---
+++ First2DGame/Objects.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First2DGame
+{
+    class Objects
+    {
+        private readonly String icon;
+
+        public Objects(string icon)
+        {
+            this.icon = icon;
+        }
+
+        public void ViewPlayer()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write(icon);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
